6a7511b703f6d1b4e5b03d2612ba70089b23f911
bestia.dev <info@bestia.dev>
2025-09-23 16:11:43 +0000

    update docs

--- docs/trait.impl/core/cmp/trait.PartialEq.js
+++ docs/trait.impl/core/cmp/trait.PartialEq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["crossplatform_path",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"crossplatform_path/enum.LibraryError.html\" title=\"enum crossplatform_path::LibraryError\">LibraryError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"crossplatform_path/struct.CrossPathBuf.html\" title=\"struct crossplatform_path::CrossPathBuf\">CrossPathBuf</a>"]]]]);
+    var implementors = Object.fromEntries([["crossplatform_path",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"crossplatform_path/enum.LibraryError.html\" title=\"enum crossplatform_path::LibraryError\">LibraryError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"crossplatform_path/struct.CrossPathBuf.html\" title=\"struct crossplatform_path::CrossPathBuf\">CrossPathBuf</a>"]]],["crossplatform_path",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"crossplatform_path/struct.CrossPathBuf.html\" title=\"struct crossplatform_path::CrossPathBuf\">CrossPathBuf</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[608]}
+//{"start":57,"fragment_lengths":[608,320]}
