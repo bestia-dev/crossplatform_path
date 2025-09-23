eccdaa9eb8cf24bf2b9c1f3e86c565884d4eac10
bestia.dev <info@bestia.dev>
2025-09-23 09:31:22 +0000

    update docs

---
+++ docs/trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["crossplatform_path",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"crossplatform_path/struct.CrossPathBuf.html\" title=\"struct crossplatform_path::CrossPathBuf\">CrossPathBuf</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.89.0/std/path/struct.PathBuf.html\" title=\"struct std::path::PathBuf\">PathBuf</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[458]}
